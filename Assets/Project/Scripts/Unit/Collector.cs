e1bce4ca49b7ec9fee8cafc5a13f6d7806a8d756
Gleb <keniomg77798@gmail.com>
2024-11-28 07:04:35 +0400

    Changed the logic of some scripts associated with units, tasks and spawners in accordance with the details of the tasks. Some scripts have been removed.

--- Assets/Project/Scripts/Unit/Collector.cs
+++ Assets/Project/Scripts/Unit/Collector.cs
@@ -3,13 +3,10 @@
 [RequireComponent(typeof(CollectorCommandController), typeof(UnitResourcesHolder))]
 public class Collector : Unit
 {
-    public CollectorCommandController CollectorCommandController { get; private set; }
-    public UnitResourcesHolder ResourcesHolder { get; private set; }
+
 
     protected override void GetComponents()
     {
         base.GetComponents();
-        ResourcesHolder = GetComponent<UnitResourcesHolder>();
-        CollectorCommandController = GetComponent<CollectorCommandController>();
     }
 }
--- Assets/Project/Scripts/Unit/Colonizator.cs
+++ Assets/Project/Scripts/Unit/Colonizator.cs
@@ -4,12 +4,10 @@
 public class Colonizator : Unit
 {
     public ColonizatorCommandController ColonizatorCommandController { get; private set; }
-    public UnitColonizer Colonizer { get; private set; }
 
     protected override void GetComponents()
     {
         base.GetComponents();
         ColonizatorCommandController = GetComponent<ColonizatorCommandController>();
-        Colonizer = GetComponent<UnitColonizer>();
     }
 }
